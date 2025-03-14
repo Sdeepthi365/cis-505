222bfa1301a334eb113307a606c274381600b226
Sdeepthi365 <ssuddala@my365.bellevue.edu>
2025-03-14 12:03:23 -0400

    Added SuddalaHelloWorld.java file.

---
+++ Assignment1.java
@@ -0,0 +1,22 @@
+/*
+ Suddala, Deepthi. (2025). CIS 505 Intermediate Java Programming.
+ Bellevue University.
+*/
+
+public class Assignment1
+{
+    /** 
+    * Method with two arguments, used to print Hello World from the firstname Deepthi and the lastname Suddala
+    * @param firstName String
+    * @param lastName String
+    * @return prints the results to the console window.
+    */
+    public static void helloworld(String firstName, String lastName)
+    {
+        System.out.print(String.format("Hello World from %s %s", firstName, lastName));
+    } //end helloWorld
+    public static void main(String[] args)
+    {
+        helloworld("Deepthi","Suddala"); //Call helloWorld method with the values "Deepthi" and "Suddala"
+    } //end main
+} //end Assignment1
